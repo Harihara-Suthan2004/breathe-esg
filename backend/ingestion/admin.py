eedfcdb220cf9e9bc3fb5ea50d180b6b17ce68a5
Harihara-suthan2004 <harigomathi20@gmail.com>
2026-05-27 20:19:56 +0530

    admin: register Organization and EmissionRecord models to admin panel dashboard

--- backend/ingestion/admin.py
+++ backend/ingestion/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Organization, EmissionRecord  
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')  
+    search_fields = ('name',)
+
+@admin.register(EmissionRecord)
+class EmissionRecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'organization', 'category_label', 'original_quantity', 'status')
+    list_filter = ('status', 'ghg_scope', 'organization')
